d53b86a23ae224b2d28bbf299bc39019d3caa897
ShreyashSingh857 <singhshreyash857@gmail.com>
2026-03-21 11:20:43 +0530

    feat(user-app): wire marketplace page to backend listings with redux state

--- apps/user-app/src/app/store.js
+++ apps/user-app/src/app/store.js
@@ -2,7 +2,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import binReducer from '../features/bins/binSlice';
 import aidetectionReducer from '../features/aidetection/aidetectionSlice';
+import marketplaceReducer from '../features/marketplace/marketplaceSlice';
 
 export const store = configureStore({
-	reducer: { auth: authReducer, bins: binReducer, aidetection: aidetectionReducer },
+	reducer: { auth: authReducer, bins: binReducer, aidetection: aidetectionReducer, marketplace: marketplaceReducer },
 });
